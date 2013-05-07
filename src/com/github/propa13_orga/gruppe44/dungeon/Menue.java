2457b0a1ee5c75cad0a3b4cc49831ce7012ac6c5
Sandy <Sandy.Weck@uni-duesseldorf.de>
2013-05-07 15:26:56 +0200

    Vergessene Menue-Datei^^

---
+++ src/com/github/propa13_orga/gruppe44/dungeon/Menue.java
@@ -0,0 +1,93 @@
+package com.github.propa13_orga.gruppe44.dungeon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.*;
+
+
+public class Menue extends JFrame implements ActionListener{
+	
+//benoetigte Buttons
+	
+	private JButton spielen;
+	private JButton einstellungen;
+	private JButton programmierer;
+	private JButton ende;
+	
+//Methoden der Buttons
+	
+	public void actionPerformed(ActionEvent a) {
+		if (a.getSource()==spielen){
+			fenster();
+		}
+		if (a.getSource()==programmierer){
+			Object[] option = {"Schliessen"};
+			
+			JOptionPane.showOptionDialog(null, "Gruppe 44","Programmierer", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, option, option [0]);
+			// null, Text, Ueberschrift, ..., Buttontext
+		}
+		
+		if (a.getSource()== einstellungen){
+			
+			//noch ohne Inhalt
+
+		}
+		
+		if (a.getSource()== ende){
+			System.exit(0);
+		}
+		
+	}
+	
+
+//Eigenschaften der Buttons 
+	
+	public Menue (String titel){
+		super(titel);
+		
+		spielen = new JButton ("Spiel starten");
+		spielen.setBounds(150,30,160,40); // Position, Groesse
+		spielen.addActionListener(this);
+		add(spielen);
+		
+		einstellungen = new JButton ("Einstellungen"); // noch ohne Inhalt
+		einstellungen.setBounds(150,110,160,40);
+		einstellungen.addActionListener(this);
+		add(einstellungen);
+		
+		programmierer = new JButton ("Programmierer");
+		programmierer.setBounds(150,190,160,40);
+		programmierer.addActionListener(this);
+		add(programmierer);
+		
+		ende = new JButton ("Spiel beenden");
+		ende.setBounds(150,270,160,40);
+		ende.addActionListener(this);
+		add(ende);
+		
+	}
+	
+// Startbildfenster
+	
+	public static void main(String[] args) {
+		
+		Menue Menue = new Menue ("Dungeon");
+		Menue.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Menue.setSize(450,400); // Fenstergroesse
+		Menue.setLayout(null); //kein vorgegebenes Layout
+		Menue.setVisible(true); //sichtbar
+	}
+	
+//Spielfenster
+	
+	public static void fenster (){
+		JFrame fenster = new JFrame ();
+		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenster.setSize(500,400);
+		fenster.setVisible (true);
+		fenster.add(new gui());
+
+	}
+
+}
