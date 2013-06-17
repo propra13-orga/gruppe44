29edecaa23eb8fefb4255ea8f0a3694871e50668
Philipp <philipp.grawe@uni-duesseldorf.de>
2013-06-17 21:33:14 +0200

    Story.java vereinfacht

--- src/folderol/Story.java
+++ src/folderol/Story.java
@@ -1,62 +1,38 @@
 package folderol;
 
+import java.awt.Component;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 public class Story {
 	private Houston houston;
 	private Map map;
-
-	private String storyUrl;
+	String storyUrl;
 	private String[] storyUrls;
-	private BufferedReader storyText;
+	BufferedReader storyBuffer;
+	String[] storyText;
 	private int mapNumber;
 	private int levelNumber;
-	private int storyCounter;
-	private int storyNumber;
 
 	public Story(int mapNumber, int levelNumber, int storyCounter) {
-
-		storyNumber = (levelNumber * 100) + (mapNumber * 10) + storyCounter;
-
-		/*
-		 * zur Veranschaulichung wie es gemeint ist. storyUrls = new String[9];
-		 * //Level 1 Raum 1 storyUrls[0] = "./res/story/story000.txt"; Raum 2
-		 * storyUrls[1] = "./res/story/story010.txt"; storyUrls[2] =
-		 * "./res/story/story020.txt"; //Level 2 storyUrls[3] =
-		 * "./res/story/story100.txt"; storyUrls[4] =
-		 * "./res/story/story110.txt"; Raum 1 mit storyCounter = 1 storyUrls[5]
-		 * = "./res/story/story111.txt"; //Level 3 storyUrls[6] =
-		 * "./res/story/story200.txt"; storyUrls[7] =
-		 * "./res/story/story210.txt"; storyUrls[8] =
-		 * "./res/story/story220.txt";
-		 */
-
 		try {
 			initializeStory();
 		} catch (IOException e) {e.printStackTrace();}
 	}
 
 	private void initializeStory() throws IOException {
-		storyNumber = (levelNumber * 100) + (mapNumber * 10) + storyCounter;
-		storyUrl = "./res/story/story" + storyNumber + ".txt";
+		storyUrl = "./res/story/story.txt";
 		readStoryFile();
+		assignFileContentToStoryText();
 
 	}
-
-	// Erhoeht den StoryCounter. Nuetzlich wenn sich der Text in einem Raum
-	// aendern soll.
-	int nextStory() {
-		storyCounter++;
-		return storyCounter;
-
-	}
-
+	
 	void renewStory(int levelNumber, int mapNumber) {
 		this.mapNumber = mapNumber;
 		this.levelNumber = levelNumber;
-		this.storyCounter = 0;
 		try {
 			initializeStory();
 		} catch (IOException e) {
@@ -68,7 +44,21 @@ public class Story {
 	private void readStoryFile() throws IOException {
 
 		FileReader fr = new FileReader(storyUrl);
-		storyText = new BufferedReader(fr);
+		storyBuffer = new BufferedReader(fr);
 
+	}
+	
+	private void assignFileContentToStoryText() {
+		String tempLine;
+		
+
+		try {
+			for (int row = 0; ((tempLine = storyBuffer.readLine()) != null); row++) {
+				storyText[row]= tempLine;
+				} 
+
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
